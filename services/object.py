469ca8bd427612079f073abac5e35e90d74104fc
Robbie Byrd <robbie@indigov.com>
2024-11-19 22:49:44 -0600

    Object Service updates

--- services/object.py
+++ services/object.py
@@ -1,11 +1,33 @@
+from models.character import Character
 from models.object import Object, ObjectUpdateDTO, ObjectCreateDTO
+from models.room import Room
 
 
 class ObjectService:
-
     @staticmethod
     def get_by_id(obj_id: str):
         return Object.objects(id=obj_id).first()
+
+    @staticmethod
+    def get_by_owner(character_id: str, show_invisible: bool = False):
+        owner = Character.objects(id=character_id).first()
+        if not owner:
+            return None
+        return Object.objects(owner=owner, visible=show_invisible)
+
+    @staticmethod
+    def get_by_holder(character_id: str, show_invisible: bool = False):
+        holder = Character.objects(id=character_id).first()
+        if not holder:
+            return None
+        return Object.objects(holder=holder, visible=show_invisible)
+
+    @staticmethod
+    def get_by_room(room_id: str, show_invisible: bool = False):
+        room = Room.objects(id=room_id).first()
+        if not room:
+            return None
+        return Object.objects(room=room, visible=show_invisible)
 
     @staticmethod
     def remove(obj_id: str):
